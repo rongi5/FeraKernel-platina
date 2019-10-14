d23eca0cbe65464c1af2443b89f67362f626dd98
feravolt <fera.volt@gmail.com>
2019-10-14 13:16:40 +0600

    Various
    Signed-off-by: feravolt <fera.volt@gmail.com>

--- env.sh
+++ env.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-export ARCH=arm64
-export SUBARCH=arm64
-export CROSS_COMPILE=/home/feravolt/misc/aarch64-raph-linux-android-elf/bin/aarch64-raphiel-elf-
-export CC_CMD=/home/feravolt/misc/aosp-clang/bin/clang
-export CLANG_TRIPLE=aarch64-linux-gnu-
-export USE_CCACHE=1
-export CCACHE_DIR=/home/feravolt/HDD/ccache
+export ARCH="arm64";export SUBARCH="arm64";export CROSS_COMPILE="/home/feravolt/misc/aarch64-raph-linux-android-elf/bin/aarch64-raphiel-elf-";export USE_CCACHE=1
 
 #  make -j4 ARCH=arm64 SUBARCH=arm64 CC="/home/feravolt/misc/aosp-clang/bin/clang" CLANG_TRIPLE="aarch64-linux-gnu-" CROSS_COMPILE="/home/feravolt/misc/aarch64-raph-linux-android-elf/bin/aarch64-raphiel-elf-"
